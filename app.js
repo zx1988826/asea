82f0c30dd4d3b114041512f571f4b641ece0102d
mac <wushain@icloud.com>
2019-12-11 20:02:32 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+
+module.exports = app => {
+  app.once('server', server => {
+    // websocket
+  });
+  app.on('error', (err, ctx) => {
+    // report error
+  });
+  app.on('request', ctx => {
+    // log receive request
+  });
+  app.on('response', ctx => {
+    // ctx.starttime is set by framework
+    const used = Date.now() - ctx.starttime;
+    // log total cost
+  });
+};
---
+++ app/middleware/robot.js
@@ -0,0 +1,25 @@
+module.exports = (options, app) => {
+  return async function robotMiddleware(ctx, next) {
+    const source = ctx.get('user-agent') || '';
+    console.log("TCL: robotMiddleware -> source", source)
+    const match = options.ua.some(ua => ua.test(source));
+    if (match) {
+      ctx.status = 403;
+      ctx.message = 'Go away, robot.';
+    } else {
+      await next();
+    }
+  }
+};
+
+// config/config.default.js
+// add middleware robot
+exports.middleware = [
+  'robot'
+];
+// robot's configurations
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
+};
